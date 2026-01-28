8d0639691cba9648c3db6a78f65b8fe95a80532e
FranManu18 <franciscopenettta@gmail.com>
2026-01-28 09:58:04 -0300

    rehecho

--- index.js
+++ index.js
@@ -1,114 +1,107 @@
-function book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function () {
-    return !read
-      ? `${this.title} by ${this.author} , ${pages} pages, not read yet`
-      : `${this.title} by ${this.author} , ${pages} pages, i readed`;
-  };
+const d=document;
+
+function Book (tittle,author,pages,read){
+    this.id=crypto.randomUUID();
+    this.tittle=tittle;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+    this.info = `The ${this.tittle} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
+};
+
+
+
+const spiderman= new Book('Spiderman','Stan lee','100',true);
+const library=[spiderman];
+
+function mostrarLibros(){
+    const $table = d.querySelector(".books");
+    $table.innerHTML = `<tr>
+    <th>Titulo</th>
+    <th>Autor</th>
+    <th>Paginas</th>
+    <th>Leido</th>`;
+
+    library.forEach((book,index)=>{
+        const booktr=d.createElement("tr");
+        booktr.innerHTML=`
+        <td>${book.tittle}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.read ? "Si" : "No"}</td>
+        <td><button class="delete" data-index="${index}">borrar libro</button></td>
+        <td><button class="status-read" data-index="${index}">Estado Leer</button></td>
+        `
+        $table.appendChild(booktr);
+    })
+
+    const $deleteButton=d.querySelectorAll(".delete")
+    $deleteButton.forEach((button)=>{
+        button.addEventListener("click",()=>{
+            let confirmation= confirm("¿Estas seguro que queres borrar ese libro?");
+            if (confirmation){
+                const indexButton= button.getAttribute("data-index");
+                borrarLibro(indexButton)
+            }
+        })
+    })
+
+    const $readButton=d.querySelectorAll(".status-read")
+    $readButton.forEach((button) => {
+        button.addEventListener("click", () =>{
+            const indexButton=button.getAttribute("data-index");
+            estadoLeido(indexButton);
+        })
+    })
+};
+mostrarLibros();
+
+function borrarLibro(index){
+    delete library[index];
+    mostrarLibros();
 }
 
-const d = document;
-
-d.addEventListener("DOMContentLoaded", () => {
-  openForm();
-  addBookToLibrary();
-});
-
-function openForm() {
-  const $button = d.querySelector(".addBook");
-
-  $button.addEventListener("click", () => {
-    const $form = d.querySelector(".formulario");
-    $form.classList.remove("hidden");
-    $button.classList.add("hidden");
-  });
+function estadoLeido(index){
+    const book=library[index];
+    book.read=!book.read;
+    mostrarLibros();
 }
-const myLibrary = [];
 
-function addBookToLibrary() {
-  const $button = d.querySelector(".addBook"),
-    $form = d.querySelector(".formulario");
 
-  $form.addEventListener("submit", (e) => {
-    e.preventDefault();
+function agregarLibro(){
+    const $button = d.querySelector(".addbook"),
+    $form=d.querySelector(".formulario");
 
-    const formData = new FormData(e.target);
+    $form.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const formData = new FormData(e.target);
 
-    const newBook = new book();
-    formData.forEach((value, key) => {
-      if (key === "read") {
-        newBook[key] = value == 1;
-      } else {
-        newBook[key] = value;
-      }
+        const tittle = formData.get("tittle");
+        const author = formData.get("author");
+        const pages = formData.get("pages");
+        const read = formData.get("read") === "1";
+
+        const newBook = new Book(tittle, author, pages, read);
+
+        library.push(newBook);
+        $form.classList.add("hidden");
+        $button.classList.remove("hidden");
+        mostrarLibros();
+        $form.reset();
     });
-
-    myLibrary.push(newBook);
-
-    $form.classList.add("hidden");
-    $button.classList.remove("hidden");
-
-    printOnPage();
-  });
 }
 
-function printOnPage() {
-  const $table = d.querySelector(".books");
-  $table.innerHTML = `<tr>
-        <th>Titulo</th>
-        <th>Autor</th>
-        <th>Paginas</th>
-        <th>Leido</th>
-      </tr>`;
-  myLibrary.forEach((book, index) => {
-    const booktr = d.createElement("tr");
-    booktr.innerHTML = `
-          <td>${book.title}</td>
-          <td>${book.author}</td>
-          <td>${book.pages}</td>
-          <td>${book.read ? "Si" : "No"}</td>
-      <td><button class="toggle-read" data-index="${index}">${
-      book.read ? "Marcar como no leído" : "Marcar como leído"
-    }</button></td>
-    <td><button class="delete" data-index="${index}">borrar libro</button></td>
-          `;
-    $table.appendChild(booktr);
-  });
-
-  const $toggleButtons = d.querySelectorAll(".toggle-read"),
-    $deleteButtons = d.querySelectorAll(".delete");
-
-  $toggleButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const bookIndex = button.getAttribute("data-index");
-      toggleChangeStatus(bookIndex);
-    });
-  });
-
-  $deleteButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      let confirmacion = confirm("Estas seguro que deseas borrar este libro?");
-      if (confirmacion) {
-        const bookIndex = button.getAttribute("data-index");
-        deleteBook(bookIndex);
-      }
-    });
-  });
+function openForm(){
+    const $button = d.querySelector(".addbook");
+    $button.addEventListener("click", ()=>{
+        const $form =d.querySelector(".formulario")
+        $form.classList.remove("hidden");
+        $button.classList.add("hidden");
+    })
 }
 
-function toggleChangeStatus(index) {
-  if (myLibrary[index].read) {
-    myLibrary[index].read = false;
-  } else {
-    myLibrary[index].read = true;
-  }
-  printOnPage();
-}
-
-function deleteBook(index) {
-  myLibrary.splice(index, 1);
-  printOnPage();
-}
+d.addEventListener("DOMContentLoaded", () =>{
+    mostrarLibros();
+    agregarLibro();
+    openForm();
+})
